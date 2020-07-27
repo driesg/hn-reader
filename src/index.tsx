088ee6a1a165e91f1e9ad7036f91891a5d03a43e
Dries Geeroms <d@driesgeeroms.com>
2020-07-27 15:11:17 +1000

    Connect UI with the business logic
    Design Choices
    ==============
    
    App is a function component so we're able to use React hooks. We use a
    useEffect hook with an empty dependency array to ensure the effect
    only happens once.
    
    We load an initial batch of stories using the generator method
    `reader.getPreviousStory()`. We use a loop to keep activating the
    generator until we've reached the inital amount of stories we want to
    load. Each time the generator produces a story we update our state so
    that the UI will be updated.
    
    When the generator is unable to produce further data we break out of
    the loop.
    
    When an error happens we show a message to the user. This can be
    viewed by blocking the domain we make the requests to in our
    browser's devtools.
    
    The loadStories method can later serve to load more stories on scroll.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./ui/App";
+import { HackerNewsClient } from "./adapters/hackernews/HackerNewsClient";
+import { NewsReader } from "./core/NewsReader";
 import * as serviceWorker from "./serviceWorker";
+import { App } from "./ui/App";
 import { ErrorBoundary } from "./ui/views/ErrorBoundary";
+
+const INITIAL_NUMBER_OF_STORIES: number = 30;
+const hnClient = new HackerNewsClient("https://hacker-news.firebaseio.com/v0");
+const newsReader = new NewsReader(hnClient);
 
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <App
+        initialNumberOfStories={INITIAL_NUMBER_OF_STORIES}
+        reader={newsReader}
+      />
     </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById("root")
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,13 +1,59 @@
-import React from "react";
-import { StoryList } from "./views/StoryList";
+import React, { useEffect, useState } from "react";
+import { NewsReader } from "../core/NewsReader";
+import { Story } from "../core/Story";
 import "./App.css";
+import { ErrorMessage } from "./views/ErrorMessage";
+import { StoryList } from "./views/StoryList";
 
-export function App() {
+interface AppProps {
+  initialNumberOfStories: number;
+  reader: NewsReader;
+}
+
+export function App({ initialNumberOfStories, reader }: AppProps) {
+  const [stories, setStories] = useState<Story[]>([]);
+  const [error, setError] = useState<string>();
+
+  async function loadStories(storiesToLoad: number): Promise<void> {
+    try {
+      const generator = reader.getPreviousStory();
+
+      while (storiesToLoad > 0) {
+        const next = await generator.next();
+
+        // break out of the for loop if the generator can't produce results anymore
+        if (next.done) {
+          // TODO (idea) set a new state value, indication there are no more messages to load
+          break;
+        }
+
+        const newStory = next.value;
+        if (newStory) {
+          setStories(function (prevState) {
+            return [...prevState, newStory];
+          });
+          storiesToLoad--;
+        }
+      }
+    } catch (error) {
+      console.log(error); // log to reporting service
+      setError(error.message);
+    }
+  }
+
+  // Disable the eslint hook so that it doesn't autofix the empty dependency array. We only want this effect to run once, hence the empty array
+  /* eslint-disable react-hooks/exhaustive-deps */
+  useEffect(function loadInitialStories() {
+    loadStories(initialNumberOfStories);
+  }, []);
+  /* eslint-enable react-hooks/exhaustive-deps */
+
   return (
     <div className="app">
       <main className="hn-reader">
         <StoryList stories={stories} title={"# Hacker News Story List"} />
       </main>
+      {error && <ErrorMessage message={error} />}
     </div>
   );
 }
